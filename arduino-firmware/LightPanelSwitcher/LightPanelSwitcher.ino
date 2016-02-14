1702342679ed183d122fd0e97341c8de6c2aaf39
Derek OKeeffe <derek0207@gmail.com>
2016-02-14 18:21:36 +0000

    change of pin

--- arduino-firmware/LightPanelSwitcher/LightPanelSwitcher.ino
+++ arduino-firmware/LightPanelSwitcher/LightPanelSwitcher.ino
@@ -1,7 +1,10 @@
 /*
- * Very basic firmware to control a relay switch for a light panel
+ * Very basic firmware to control a relay switch for a light panel.
+ * Relay connected to pin 14
  */
 #include <Firmata.h>
+
+const int RELAY_PIN =  12;
 
 /**
  * Interpret the command string and set the pin state.
@@ -10,17 +13,17 @@ void stringCallback(char *myString)
 {
   String commandString = String(myString);
   if (commandString.equals("ON")) {
-    digitalWrite(13, HIGH);
+    digitalWrite(RELAY_PIN, HIGH);
   } else if (commandString.equals("OFF")) {
-    digitalWrite(13, LOW);
+    digitalWrite(RELAY_PIN, LOW);
   } 
   Firmata.sendString(myString);
 }
 
 void setup()
 {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
   Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
   Firmata.attach(STRING_DATA, stringCallback);
   Firmata.begin(57600);
